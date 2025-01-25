fd2619b5d153c492fde6d9a18de60e7d30046853
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-01-25 14:57:08 -0500

    fix mod not working on server because of rendering code running serverside

--- src/main/java/net/natsupotato/acidboy/AcidicBoyfriendsClient.java
+++ src/main/java/net/natsupotato/acidboy/AcidicBoyfriendsClient.java
@@ -1,19 +1,20 @@
 package net.natsupotato.acidboy;
 
 import net.mine_diver.unsafeevents.listener.EventListener;
-import net.modificationstation.stationapi.api.event.entity.EntityRegister;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.modificationstation.stationapi.api.client.event.render.entity.EntityRendererRegisterEvent;
 import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
 import net.modificationstation.stationapi.api.util.Namespace;
 import net.modificationstation.stationapi.api.util.Null;
 
-public class AcidicBoyfriends {
+public class AcidicBoyfriendsClient {
 
     @Entrypoint.Namespace
     public static final Namespace NAMESPACE = Null.get();
 
     @EventListener
-    public void registerEntities(EntityRegister event) {
+    public void registerEntityRenderer(EntityRendererRegisterEvent event) {
 
-        event.register(BoyfriendEntity.class, "boyfriend");
+        event.renderers.put(BoyfriendEntity.class, new LivingEntityRenderer(new BoyfriendEntityModel(), 0.5f));
     }
 }
